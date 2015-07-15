99808ea70f47478107829a4fa726202a886798ba
Gert Geidel <gert.geidel@infopark.de>
2015-07-15 13:59:40 +0200

    More Meta Attributes

--- app/concerns/meta_data_extender.rb
+++ app/concerns/meta_data_extender.rb
@@ -3,13 +3,12 @@ module MetaDataExtender
     base.attribute :meta_description, :string
     base.attribute :meta_keywords, :string
     base.attribute :meta_author, :string
-    base.attribute :meta_keywords, :string
     base.attribute :meta_publisher, :string
     base.attribute :meta_copyright, :string
 
     base.attribute :meta_page_topic, :string
     base.attribute :meta_page_type, :string
-    base.attribute :meta_audience, :string
-    base.attribute :meta_robots, :enum, values: ["index", "noindex", "nofollow", "all"]
+    base.attribute :meta_audience, :multienum , values: ["all", "adult", "advanced", "artist", "business", "child", "college", "craft", "elementary", "elhi", "government", "grownup", "health", "high", "institution", "intermediate", "introductory", "law", "lawyer", "listeners", "military", "news", "older", "parent", "patient", "popular", "scholarly", "teacher", "tts", "viewers"]
+    base.attribute :meta_robots, :multienum, values: ["noindex", "nofollow", "noarchive", "nosnippet", "noimageindex", "notranslate"]
   end
 end
